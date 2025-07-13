abca9f8ce2ce5964098f57897a62d9dfe221dd8b
RitikaFullStackDeveloper <charperitika@gmail.com>
2025-07-12 23:05:27 -0700

    Add files via upload

---
+++ Code/english.js
@@ -0,0 +1,8 @@
+const englishData = {
+    heading: "Virat Kohli",
+    information: `Virat Kohli (born 5 November 1988) is an Indian international cricketer and the former captain of the Indian national cricket team.
+  He is a right-handed batsman and an occasional medium-fast bowler. He currently represents Royal Challengers Bengaluru in the IPL and Delhi in domestic cricket.
+  Kohli is widely regarded as one of the greatest batters of all time. He also holds the record for most centuries in ODI cricket and is second in the list of most international centuries,
+  and is the highest run-scorer in IPL. Kohli was a member of the Indian team that won the 2011 Cricket World Cup, 2013 ICC Champions Trophy, 2024 T20 World Cup and 2025 Champions Trophy.
+  He captained India to win the ICC Test mace three consecutive times in 2017, 2018, and 2019.`
+  };
---
+++ Code/spanish.js
@@ -0,0 +1,8 @@
+const spanishData = {
+    heading: "Virat Kohli",
+    information: `Virat Kohli (nacido el 5 de noviembre de 1988) es un jugador de críquet internacional indio y ex capitán del equipo nacional de críquet de India.
+  Es un bateador diestro y ocasionalmente lanza medio-rápido. Actualmente representa a Royal Challengers Bengaluru en la IPL y a Delhi en el cricket doméstico.
+  Kohli es ampliamente considerado como uno de los mejores bateadores de todos los tiempos. También tiene el récord de más siglos en ODI y es el segundo con más siglos internacionales.
+  Es el máximo anotador de carreras en la IPL. Kohli fue miembro del equipo indio que ganó la Copa Mundial de Críquet 2011, el Trofeo de Campeones 2013, la Copa Mundial T20 2024 y el Trofeo de Campeones 2025.
+  Capitaneó a India para ganar el trofeo ICC Test Mace tres veces consecutivas en 2017, 2018 y 2019.`
+  };
